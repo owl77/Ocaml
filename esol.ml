c079f06f7a6a15d78846dc4e385ca69a8a4b4c54
owl77 <41900928+owl77@users.noreply.github.com>
2024-12-17 17:22:29 +0000

    Add files via upload

--- esol.ml
+++ esol.ml
@@ -1,4 +1,8 @@
 
 type argumentSig = PredicateSig of int | IndividualSig;;
-type formula = App of term*(term list) | And of formula*formula | Or of formula*formula | Neg of formula | Imp of formula*formula | Forall of string* argumentSig | Exists of string* argumentSig 
-and term = Constant of string*argumentSig | Variable of string*argumentSig | Lambda of (term list)* formula;;
+type formula = App of term*(term list) | 
+And of formula*formula | Or of formula*formula | 
+Neg of formula | Imp of formula*formula | 
+Forall of string* argumentSig | Exists of string* argumentSig 
+and term = Constant of string*argumentSig | 
+Variable of string*argumentSig | Lambda of (term list)* formula;;
